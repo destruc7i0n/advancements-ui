a384ef67ef3b03b6e846c3b5b374958354eec090
destruc7i0n <dscdsouza@outlook.com>
2017-11-08 16:34:35 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({
+    icon: path.join(__dirname, 'icon.ico'),
+    show: false,
+    backgroundColor: '#eee',
+    webPreferences: {
+      zoomFactor: 0.75,
+      experimentalFeatures: true
+    }
+  })
+  win.setMenu(null)
+  win.maximize()
+  win.show()
+  win.loadURL('https://advancements.thedestruc7i0n.ca')
+  win.openDevTools()
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
